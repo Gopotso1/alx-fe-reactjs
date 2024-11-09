c44c0a8c779f7608a2e0d51b7ff19e3d14b7227f
Gopotso1 <maphosasegopotso1@gmail.com>
2024-11-09 21:47:02 +0200

    Update Footer.jsx

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -2,10 +2,35 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '10px' }}>
-      <p style={{ margin: '0' }}>© 2024 My Favorite Cities. All rights reserved.</p>
+    <footer
+      style={{
+        backgroundColor: '#333',
+        color: 'white',
+        textAlign: 'center',
+        padding: '20px',
+        marginTop: '30px',
+        fontFamily: "'Arial', sans-serif",
+      }}
+    >
+      <p
+        style={{
+          fontSize: '16px',
+          fontWeight: 'bold',
+          marginBottom: '10px',
+        }}
+      >
+        &copy; 2024 My Favorite Cities
+      </p>
+      <p
+        style={{
+          fontSize: '14px',
+          color: '#bbb',
+        }}
+      >
+        Made with ❤️ by Maphosa Segopotso
+      </p>
     </footer>
   );
-}
+};
 
 export default Footer;
