e1e4758267f7df2843f9268216a400b07d931a67
Maphosa Segopotso <maphosasegopotso1@gmail.com>
2024-11-10 16:03:58 +0200

    updated NavBar.jsx

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  const navbarStyle = {
+    display: 'flex',
+    justifyContent: 'space-around',
+    background: '#333',
+    padding: '10px',
+    color: '#fff'
+  };
+
+  return (
+    <nav style={navbarStyle}>
+      <Link to="/" style={{ color: '#fff', textDecoration: 'none' }}>Home</Link>
+      <Link to="/about" style={{ color: '#fff', textDecoration: 'none' }}>About</Link>
+      <Link to="/services" style={{ color: '#fff', textDecoration: 'none' }}>Services</Link>
+      <Link to="/contact" style={{ color: '#fff', textDecoration: 'none' }}>Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
