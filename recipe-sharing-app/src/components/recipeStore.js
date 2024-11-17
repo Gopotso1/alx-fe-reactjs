bcf17100b72428f423b2ce30c9380e4695ca1dfd
Maphosa Segopotso <maphosasegopotso1@gmail.com>
2024-11-17 17:46:35 +0200

    updated recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,6 +2,16 @@ import { create } from 'zustand';
 
 const useRecipeStore = create(set => ({
   recipes: [],
+  searchTerm: '',
+  filteredRecipes: [],
+  setSearchTerm: (term) => {
+    set({ searchTerm: term });
+    set(state => ({
+      filteredRecipes: state.recipes.filter(recipe =>
+        recipe.title.toLowerCase().includes(term.toLowerCase())
+      )
+    }));
+  },
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   deleteRecipe: (id) => set(state => ({ recipes: state.recipes.filter(recipe => recipe.id !== id) })),
   updateRecipe: (updatedRecipe) => set(state => ({
@@ -9,7 +19,8 @@ const useRecipeStore = create(set => ({
       recipe.id === updatedRecipe.id ? updatedRecipe : recipe
     )
   })),
-  setRecipes: (recipes) => set({ recipes })
+  setRecipes: (recipes) => set({ recipes, filteredRecipes: recipes })
 }));
 
 export default useRecipeStore;
+
