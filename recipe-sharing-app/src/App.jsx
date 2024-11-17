ee19ba655fcad92b4d3e07d9370fe2cc1e431b05
Maphosa Segopotso <maphosasegopotso1@gmail.com>
2024-11-17 18:37:46 +0200

    updated Recommendations and FavoriteList to Main layout

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -4,7 +4,8 @@ import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
 import SearchBar from './components/SearchBar';
-
+import FavoritesList from './components/FavoriteLists';
+import RecommendationsList from './components/RecommendationsList';
 
 const App = () => {
   return (
