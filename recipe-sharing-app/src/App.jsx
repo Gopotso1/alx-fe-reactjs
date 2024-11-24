15bf756839cbe048ab2c942531f3f92248f549eb
Maphosa Segopotso <maphosasegopotso1@gmail.com>
2024-11-24 10:00:42 +0200

    add

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
         <SearchBar />
         <AddRecipeForm />
 
-        //added 
+        //added but remove to run
         <FavoritesList />
         <RecommendationsList />
        
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,6 +1,3 @@
-
-
-
 const useRecipeStore = create(set => ({
   recipes: [],
   favorites: [],
