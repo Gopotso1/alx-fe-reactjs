6d561a4adc8d718a7e80dffd645f5ae8fc636624
Maphosa Segopotso <maphosasegopotso1@gmail.com>
2024-11-28 20:12:51 +0200

    add

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -11,7 +11,7 @@ const UserProfile = () => {
           className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 rounded-full border-4 border-gray-300"
         />
 
-        {/* User info */}
+        {/* User Info */}
         <div className="mt-4 text-center">
           <h2 className="text-lg sm:text-xl md:text-2xl font-semibold text-gray-800">John Doe</h2>
           <p className="text-sm sm:text-base md:text-lg text-gray-600 mt-2">
