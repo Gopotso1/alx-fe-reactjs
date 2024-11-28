f3415ed20b67e94e8a55b9ac36bbee9c37a60c6a
Maphosa Segopotso <maphosasegopotso1@gmail.com>
2024-11-28 20:48:11 +0200

    add

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -8,7 +8,7 @@ const UserProfile = () => {
         <img 
           src="https://via.placeholder.com/150" 
           alt="User Profile" 
-          className="sm:w-24 sm:h-24 w-32 h-32 md:w-36 md:h-36 rounded-full border-4 border-gray-300 transition-transform duration-300 ease-in-out hover:scale-110"
+          className="sm:w-24 sm:h-24 w-32 h-32 md:w-36 md:h-36 rounded-full border-4 border-gray-300 hover:scale-110 transition-transform duration-300 ease-in-out"
         />
 
         {/* User Information */}
