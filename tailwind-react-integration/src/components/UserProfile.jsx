a6887bf103f8f6d1649b69d20205226d1756845e
Maphosa Segopotso <maphosasegopotso1@gmail.com>
2024-11-28 20:36:30 +0200

    add

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const UserProfile = () => {
   return (
-    <div className="max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg mx-auto sm:p-4 md:p-8 lg:p-12">
+    <div className="max-w-xs sm:max-w-sm md:max-w-sm lg:max-w-lg mx-auto sm:p-4 md:p-8 lg:p-12">
       <div className="flex flex-col items-center">
         {/* Profile Image */}
         <img 
@@ -11,7 +11,7 @@ const UserProfile = () => {
           className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 rounded-full border-4 border-gray-300"
         />
 
-        {/* User Info */}
+        {/* User Information */}
         <div className="mt-4 text-center">
           <h2 className="text-lg sm:text-xl md:text-2xl font-semibold text-gray-800">John Doe</h2>
           <p className="text-sm sm:text-base md:text-lg text-gray-600 mt-2">
