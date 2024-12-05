2906ba9d6759cad32cf4cb3be8301ec143021622
Maphosa Segopotso <maphosasegopotso1@gmail.com>
2024-12-05 18:18:53 +0200

    Created HomePage with recipe list and responsive layout

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,8 +1,10 @@
+import React from 'react';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
-    <div className="bg-gray-100 h-screen flex justify-center items-center">
-      <h1 className="text-blue-500 text-4xl">Welcome to the Recipe Sharing Platform!</h1>
+    <div>
+      <HomePage />
     </div>
   );
 }
